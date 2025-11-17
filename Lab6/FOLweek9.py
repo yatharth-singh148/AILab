70ca9371b0b86fa3f1afddb69dad1b02c41e08fa
yatharth-singh148 <yatharthsingh.cs23@bmsce.ac.in>
2025-11-17 17:08:31 +0530

    Update FOLweek9.py

--- Lab6/FOLweek9.py
+++ Lab6/FOLweek9.py
@@ -1,3 +1,4 @@
+# Simple Simulation of FOL Resolution
 # Goal: Prove that John likes peanuts
 
 # Knowledge Base
@@ -6,25 +7,32 @@ food = []
 eats = []
 alive = []
 not_killed = []
-# Given facts from PDF
+
+# Given facts
 food.append("apple")
 food.append("vegetable")
 
 eats.append(("anil", "peanuts"))
 alive.append("anil")
+
 # Harry eats everything Anil eats
 def harry_eats_all():
-    for (_, item) in eats:
+    # Make a copy to avoid modifying while iterating
+    current_eats = eats.copy()
+    for (_, item) in current_eats:
         eats.append(("harry", item))
 
 harry_eats_all()
+
 # Anyone alive → not killed
 for x in alive:
     not_killed.append(x)
 
 # Anyone not killed → alive
 for x in not_killed:
-    alive.append(x)
+    if x not in alive:
+        alive.append(x)
+
 # If someone eats something & is not killed → that thing is food
 def derive_food():
     for (person, item) in eats:
@@ -32,13 +40,17 @@ def derive_food():
             food.append(item)
 
 derive_food()
+
 # John likes all food
 def derive_likes():
     for item in food:
         likes.append(("john", item))
+
 derive_likes()
+
 # Query we want to prove
 query = ("john", "peanuts")
+
 # Resolution-like check
 if query in likes:
     print("Conclusion: John likes peanuts (PROVED).")
